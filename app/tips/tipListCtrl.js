d326bd66b459a2225631fa8b65307e3fc7717c2b
Jessica <jessica@strangepunch.com>
2015-03-06 15:18:39 -0500

    fixed the Prev and Next button display for first and last page

--- app/tips/tipListCtrl.js
+++ app/tips/tipListCtrl.js
@@ -27,16 +27,37 @@
 			//console.log("vm.currentTip", vm.currentTip);
 		});
 
+		//start the page with no previous page buton
+		vm.noPrev = true;
+
 		//go to next array of data
 		$scope.nextPage = function(){
 			//console.log("in nextPage");
 
+			//turn on both button when there is pages before or after this page
+			vm.noPrev = false;
+			vm.noNext = false;
+
 			//as long as current page number is less than length of array of data
     		if (vm.currentPage < vm.tips.length-1) {
+
     			vm.currentPage++; //increment forward 1	
+
       			//console.log("count",vm.currentPage);
+
       			tipResource.query(function(data){
+
+      				//turn off next button when there is no next,
+      				//which is the last page
+      				if(vm.currentPage == vm.tips.length-1)
+      				{
+      					vm.noNext = true;
+      					vm.noPrev = false; //keep prev button on.
+      				}
+
       				//console.log("data next", data[vm.currentPage]);
+
+      				//return current tip value
 					return vm.currentTip = data[vm.currentPage];
 				});
     		}
@@ -45,14 +66,28 @@
 
 		//go to previous array of data
   		$scope.prevPage = function() {
+
     		//console.log("in prevPage");
+
+    		//turn on both button when there is pages before or after this page
+      		vm.noNext = false;
+      		vm.noPrev = false;
 
     		//as long as current page number not less than 0
     		if (vm.currentPage > 0) {
     			vm.currentPage--; //increment backward 1
       			//console.log("count",vm.currentPage);
       			tipResource.query(function(data){
+
+      				//turn off prev button when current page number is 0,
+      				//which is the first page.
+      				if(vm.currentPage == 0){
+      					vm.noPrev = true;
+      				}
+
       				//console.log("data prev", data[vm.currentPage]);
+
+      				//return current tip value
 					return vm.currentTip = data[vm.currentPage];
 				});
     		}
